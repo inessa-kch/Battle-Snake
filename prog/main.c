b8c2fba8b053f5596284da9cf192515cf4025a02
inessthemess <inessa.kechek@etu.sorbonne-universite.fr>
2024-05-01 01:31:21 +0200

    snake v2: the player can select his move during each round

--- prog/main.c
+++ prog/main.c
@@ -6,8 +6,9 @@
 
 
 int main(){
+    
+	connectToServer("localhost",1234,"bli");
     printf("bliblou\n");
-	connectToServer("localhost",1234,"bla");
     char gameName[50];
     int sizeX;
     int sizeY;
@@ -17,23 +18,40 @@ int main(){
     t_move x;
     t_return_code moveSent;
     t_return_code moveReceived;
+    
     waitForSnakeGame("TRAINING SUPER_PLAYER",gameName,&sizeX,&sizeY,&nbWalls);
+    
+    int moveInt;
+    t_move move;
     
     int a=getSnakeArena(walls);
     //int b=getSnakeArena(walls);
+    
     while (Gamerunning){
+        
         if (a==0){
-            a=1;
-            moveSent=sendMove(NORTH);
             
-            if (moveSent == LOSING_MOVE) Gamerunning=0;
+            a=1;
+            printf("Enter a move (0 for NORTH, 1 for EAST, 2 for SOUTH, 3 for WEST): \n");
+            scanf("%d", &moveInt);
+            printf("\n");
+            move = (t_move)moveInt;
+            moveSent=sendMove(move);
+            
+            if (moveSent == LOSING_MOVE) {
+                printf("You lost :(\n");
+                Gamerunning=0;
+            }
             else printArena();
            
         }
         else if (a==1){
             a=0;
             moveReceived=getMove(&x);
-            if (moveReceived == LOSING_MOVE) Gamerunning=0;
+            if (moveReceived == LOSING_MOVE){
+                printf("You won!! :)\n");
+                 Gamerunning=0;
+            }
             else printArena();
             
         }
